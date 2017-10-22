1fe0771904e10cd67929ddcef27171ca57c2daa9
parker1220 <nguyenphucluc1220@gmail.com>
2017-10-22 16:30:27 +0700

    Bai 2-3-4

---
+++ LucNguyenPhuc_Buoi4/Bai2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAdress;
+            int port;
+            string protocol;
+            Console.Write("Nhap Host IP: ");
+            ipAdress = Console.ReadLine();
+            Console.Write("Nhap Host Port: ");
+            port = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap protocol: ");
+            protocol = Console.ReadLine();
+
+            switch (protocol)
+            {
+                case "TCP":
+                    IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+                    TcpClient client = new TcpClient();
+                    try
+                    {
+                        client.Connect(ip);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Host: {0}:{1} is close", ipAdress, port);
+                        Console.WriteLine(ex);
+                        Environment.Exit(0);
+
+                    }
+                    Console.WriteLine("Host: {0}:{1} is open", ipAdress, port);
+                    break;
+                case "UDP":
+                    break;
+            }
+        }
+    }
+}
---
+++ LucNguyenPhuc_Buoi4/Bai3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress[] ippool = new IPAddress[5];
+            int r;
+            Random rd = new Random();
+            for (int i = 0; i < ippool.Length; i++)
+            {
+                ippool[i] = IPAddress.Parse("192.168.1." + (i + 10).ToString());
+            }
+            IPEndPoint ip = new IPEndPoint(IPAddress.Any, 9050);
+            UdpClient udps = new UdpClient(ip);
+
+            Console.WriteLine("Waiting for a client...");
+
+            Byte[] data = new Byte[1024];
+            data = udps.Receive(ref ip);
+            if (Encoding.ASCII.GetString(data) == "req")
+            {
+                r = rd.Next(0, ippool.Length);
+                udps.Send(Encoding.ASCII.GetBytes(ippool[r].ToString()), ippool[r].ToString().Length, ip);
+                Console.WriteLine("Da gui IP cho client");
+            }
+            data = new Byte[1024];
+            data = udps.Receive(ref ip);
+            if (Encoding.ASCII.GetString(data) == "ack")
+            {
+                udps.Send(Encoding.ASCII.GetBytes("fin"), 3, ip);
+                Console.WriteLine("Server ket thuc DHCP cho client");
+            }
+            Console.ReadLine();
+        }
+    }
+}
